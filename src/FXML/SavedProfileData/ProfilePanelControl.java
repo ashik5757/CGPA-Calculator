6d391a51eefbbdb6a940e27a5eedb8544b84df49
Ashik Rahman <81816852+ashik5757@users.noreply.github.com>
2021-11-30 00:45:04 +0600

    Alert

--- src/FXML/SavedProfileData/ProfilePanelControl.java
+++ src/FXML/SavedProfileData/ProfilePanelControl.java
@@ -9,6 +9,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -52,12 +54,22 @@ public class ProfilePanelControl implements Initializable {
     @FXML
     void home(ActionEvent event) throws IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Homepage2.fxml"));
-        Parent root = loader.load();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Log Out");
+        alert.setHeaderText("You are about to EXIT the profile");
+        alert.setContentText("Are you sure??");
 
-        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
+        if (alert.showAndWait().get() == ButtonType.OK) {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Homepage2.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+
+
 
     }
 
